583d014866188fc9e8b9f472c78fc7d72bb2a125
whitakerchancellor <whitakerchancellor@yahoo.com>
2021-04-21 10:35:54 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import ProductsPage from './ProductsPage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ProductsPage></ProductsPage>
+    </>
   );
 }
 
---
+++ src/ProductsPage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Product from './Product.js';
+
+function ProductsPage() {
+  return (
+    <>
+      <h1>Products</h1>
+    </>
+  );
+}
+
+export default ProductsPage;
